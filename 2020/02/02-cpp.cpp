3dbe1a3f0191e8db03729b820fea2a4d27464094
lavaloid <36921951+lavaloid@users.noreply.github.com>
2020-12-05 16:53:48 +0800

    Fixing indentation

--- 2020/02/02-cpp.cpp
+++ 2020/02/02-cpp.cpp
@@ -8,30 +8,33 @@ int main() {
     int result = 0;
     
     while (f.peek() != EOF) {
-    int min = 0;
-    char last = '\0';
-    while (true) {
-        f.get(last);
-        if (last == '-') break;
-        min *= 10;
-        min += last - '0';
-    }
-    int max = 0;
-    while (true) {
-        f.get(last);
-        if (last == ' ') break;
-        max *= 10;
-
-        max += last - '0';
-    }
-    char find; f.get(find); f.get(); f.get();
-    int count = 0;
-    while (true) {
-        f.get(last);
-        if (last == '\n') break;
-        if (last == find) count++;
-    }
-    if (count >= min && count <= max) result++;
+        int min = 0;
+        char last = '\0';
+        while (true) {
+            f.get(last);
+            if (last == '-') break;
+            min *= 10;
+            min += last - '0';
+        }
+        
+        int max = 0;
+        while (true) {
+            f.get(last);
+            if (last == ' ') break;
+            max *= 10;
+            max += last - '0';
+        }
+        
+        char find; f.get(find); f.get(); f.get();
+        
+        int count = 0;
+        while (true) {
+            f.get(last);
+            if (last == '\n') break;
+            if (last == find) count++;
+        }
+        
+        if (count >= min && count <= max) result++;
     }
     cout << result << endl;
     f.close();
