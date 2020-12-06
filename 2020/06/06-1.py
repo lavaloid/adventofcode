6582275d5976c6b96a8153c52b6d124eded090fa
lavaloid <36921951+lavaloid@users.noreply.github.com>
2020-12-06 13:42:07 +0800

    Day 6

---
+++ 2020/06/06-1.py
@@ -0,0 +1,28 @@
+import time
+
+start_time = int(round(time.time() * 1000))
+
+# Input nonsense
+f = open("06.txt", "r")
+input_str = f.read().split('\n')
+
+# Fun logic
+data = [False] * 26
+count = 0
+
+for line in input_str:
+    if line == '':
+        for i in range(26):
+            if data[i]:
+                count += 1
+            data[i] = False
+        continue
+
+    for ch in line:
+        data[ord(ch) - ord('a')] = True
+
+print(count)
+
+end_time = int(round(time.time() * 1000))
+
+print(f"Time: {end_time - start_time} ms")
---
+++ 2020/06/06-2.py
@@ -0,0 +1,31 @@
+import time
+
+start_time = int(round(time.time() * 1000))
+
+# Input nonsense
+f = open("06.txt", "r")
+input_str = f.read().split('\n')
+
+# Fun logic
+data = [0] * 26
+count = 0
+group_member_no = 0
+
+for line in input_str:
+    if line == '':
+        for i in range(26):
+            if data[i] == group_member_no:
+                count += 1
+            data[i] = 0
+        group_member_no = 0
+        continue
+
+    group_member_no += 1
+    for ch in line:
+        data[ord(ch) - ord('a')] += 1
+
+print(count)
+
+end_time = int(round(time.time() * 1000))
+
+print(f"Time: {end_time - start_time} ms")
